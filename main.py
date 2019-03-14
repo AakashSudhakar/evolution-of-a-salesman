944e9fe96abcada6eaf7a399ec22abcf2b6121d9
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-03-13 17:38:18 -0700

    Implemented basic City object with distance calc

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class City(object):
     """ Object architecture that contains structural logic for our city representation. """
     def __init__(self, x_pos, y_pos):
         """
-        Initializing our City class object with rectangular coordinate data.
+        Helper method to initialize our City class object with rectangular coordinate data.
 
         INPUT(S):
             - x_pos {float}
@@ -37,3 +37,26 @@ class City(object):
         """
         self.x_pos = x_pos
         self.y_pos = y_pos
+
+    def __repr__(self):
+        """
+        Helper method to cleanly render self-contained coordinate data.
+
+        INPUT(S):
+            N/A
+        OUTPUT(S):
+            - {str}
+        """
+        return "({}, {})".format(str(self.x_pos), str(self.y_pos))
+
+    def calculate_euclidean_distance(self, next_city):
+        """
+        Method to calculate distance between current and next city instances using Euclidean distance formula.
+
+        INPUT(S):
+            - next_city {City(object)}
+        OUTPUT(S):
+            - {float}
+        """
+        distance_x, distance_y = abs(self.x_pos - next_city.x_pos), abs(self.y_pos - next_city.y_pos)
+        return np.sqrt((distance_x ** 2) + (distance_y ** 2))
